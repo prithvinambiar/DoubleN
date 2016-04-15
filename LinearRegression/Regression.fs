cac563b700e5818e7c2626c3992c0ecc529479c0
prithvinambiar <prithvinambiar@gmail.com>
2016-04-15 10:54:49 +0530

    Using seq instead of list to improve performance

--- LinearRegression/Regression.fs
+++ LinearRegression/Regression.fs
@@ -6,24 +6,24 @@ let random = Random()
 
 let sigmoid xi theta =
     xi
-    |> List.fold2(fun s a b -> s + a * b) 0.0 theta
+    |> Seq.fold2(fun s a b -> s + a * b) 0.0 theta
     |> fun res -> 1.0 / (1.0 + Math.Exp -res)
 
 let step x y theta j =
     y
-    |> List.map2(fun xi yi -> ((sigmoid xi theta) - yi) * xi.[j]) x
-    |> List.reduce(+)
+    |> Seq.map2(fun xi yi -> ((sigmoid xi theta) - yi) * (xi |> Seq.item j)) x
+    |> Seq.reduce(+)
 
-let updateTheta (x:float list list) y theta alpha =
+let updateTheta (x:float seq seq) y theta alpha =
     let step' = step x y theta
     theta
-    |> List.mapi(fun i t -> t - alpha * (step' i))
+    |> Seq.mapi(fun i t -> t - alpha * (step' i))
 
 let cost x y theta=
     y
-    |> List.map2(fun xi yi -> yi * -Math.Log (sigmoid xi theta) + (1.0 - yi) * -Math.Log (1.0 - (sigmoid xi theta))) x
-    |> List.reduce(+)
-    |> fun a -> a / (float)x.Length
+    |> Seq.map2(fun xi yi -> yi * -Math.Log (sigmoid xi theta) + (1.0 - yi) * -Math.Log (1.0 - (sigmoid xi theta))) x
+    |> Seq.reduce(+)
+    |> fun a -> a / (float)(x |> Seq.length)
 
 let isConverged (cost:float) (prevCost:float) =
     let threshold = 0.0001
@@ -40,15 +40,16 @@ let rec run x y theta iter prevCost =
         | true -> printfn "Logistic regression has converged, cheers!!. Number of iteration %A" iter |> ignore; theta
         | false -> run x y theta (iter + 1) cost'
     | false ->
-        printfn "Logistic regression didn't converge, sad :-(. Consider increasing the number of max iteration."
+        printfn "Logistic regression didn't converge, sad :-(. Re-consider the alpha value and max number of iterations."
         theta
 
-let regress (x:float list list) y =
+let regress (x:float seq seq) y =
     let x = 
         x
-        |> List.map(fun xi -> xi |> List.append([1.0]))
+        |> Seq.map(fun xi -> xi |> Seq.append([1.0]))
     let theta =
-        x.[0]
-        |> List.map(fun _ -> random.NextDouble())
+        x
+        |> Seq.head
+        |> Seq.map(fun _ -> random.NextDouble())
     
     run x y theta 0 0.0
