5ab4321c297ef49d0f5d73fe80a269bbf7a5b648
prithvinambiar <prithvinambiar@gmail.com>
2017-01-22 23:20:42 +0530

    Added train module. Added functions to calculate cost and costDerivative.

---
+++ DoubleN/Train.fs
@@ -0,0 +1,20 @@
+﻿module DoubleN.Train
+
+let cost actual predicted =
+    let squaredError a b =
+        (b - a) ** 2.0
+    let m = actual |> Seq.length |> float
+
+    actual
+    |> Seq.map2 squaredError predicted
+    |> Seq.reduce(+)
+    |> fun res -> res / (2.0 * m)
+
+let costDerivative input actual predicted =
+    let m = actual |> Seq.length |> float
+
+    actual
+    |> Seq.map2(-) predicted
+    |> Seq.map2(*) input
+    |> Seq.reduce(+)
+    |> fun res -> res / m
